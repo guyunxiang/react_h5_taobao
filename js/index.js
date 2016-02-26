998e5f07d5b3c2013b5c7cdc0673f21908855d70
guyunxiang <guyunxiang32@gmail.com>
2016-02-26 18:09:44 +0800

    添加react.js

---
+++ js/index.js
@@ -0,0 +1,11 @@
+/**
+ * User: guyunxiang
+ * Date: 16-2-26
+ * Time: 下午5:53
+ * Description:TODO
+ */
+ReactDOM.render(React.createElement(
+  'h1',
+  null,
+  'Hello World'
+), document.getElementById('root'));
---
+++ src/index.js
@@ -0,0 +1,10 @@
+/**
+ * User: guyunxiang
+ * Date: 16-2-26
+ * Time: 下午5:53
+ * Description:TODO
+ */
+ReactDOM.render(
+  <h1>Hello World</h1>,
+  document.getElementById('root')
+);
