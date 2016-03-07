451d121006ae54704071b62c077c3ebdcb6f9188
guyunxiang <guyunxiang32@gmail.com>
2016-03-07 16:58:05 +0800

    fixed

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@
  */
 
 var HeaderComponent = React.createClass({
-  render(){
+  render() {
     return (
       <header class="ui-header ui-header-stable ui-border-b">
         <div className="header">
@@ -21,20 +21,20 @@ var HeaderComponent = React.createClass({
 
 var BannerComponent = React.createClass({
 
-  componentDidMount(){
-    window.addEventListener('load', function () {
-      var slider = new fz.Scroll('.ui-slider', {
-        role: 'slider',
-        indicator: true,
-        autoplay: true,
-        interval: 5000
-      });
-    })
-  },
+  componentDidMount() {
+      window.addEventListener('load', function() {
+        var slider = new fz.Scroll('.ui-slider', {
+          role: 'slider',
+          indicator: true,
+          autoplay: true,
+          interval: 5000
+        });
+      })
+    },
 
-  render(){
-    return (
-      <div className="ui-slider">
+    render() {
+      return (
+        <div className="ui-slider">
         <ul className="ui-slider-content">
           <li className="current">
             <img src="images/TB1cMD8LFXXXXbLXXXXSutbFXXX.jpg_q50.jpg" alt=""/>
@@ -47,43 +47,43 @@ var BannerComponent = React.createClass({
           </li>
         </ul>
       </div>
-    )
-  }
+      )
+    }
 });
 
 var ModulesComponent = React.createClass({
   // 初始化头条内容
-  getInitialState(){
-    return {
-      type: '最新',
-      text: '刘海造型 这个样子更美！'
-    }
-  },
-  // 定时更改头条滚动字母
-  componentDidMount(){
-    var toutiaoArray = [{
-      "title": "最热",
-      "text": "宅在家里，最正确的穿着"
-    }, {
-      "title": "推荐",
-      "text": "川剧有句话叫...绝活"
-    }, {
-      "title": "最新",
-      "text": "刘海造型 这个样子更美！"
-    }];
-    var i = 0;
-    this.timer = setInterval(function () {
-      (i > 2) ? i = 0 : 0;
-      this.setState({
-        type: toutiaoArray[i]['title'],
-        text: toutiaoArray[i]['text']
-      });
-      i = i + 1;
-    }.bind(this), 3000);
-  },
-  render() {
-    return (
-      <div>
+  getInitialState() {
+      return {
+        type: '最新',
+        text: '刘海造型 这个样子更美！'
+      }
+    },
+    // 定时更改头条滚动字母
+    componentDidMount() {
+      var toutiaoArray = [{
+        "title": "最热",
+        "text": "宅在家里，最正确的穿着"
+      }, {
+        "title": "推荐",
+        "text": "川剧有句话叫...绝活"
+      }, {
+        "title": "最新",
+        "text": "刘海造型 这个样子更美！"
+      }];
+      var i = 0;
+      this.timer = setInterval(function() {
+        (i > 2) ? i = 0: 0;
+        this.setState({
+          type: toutiaoArray[i]['title'],
+          text: toutiaoArray[i]['text']
+        });
+        i = i + 1;
+      }.bind(this), 3000);
+    },
+    render() {
+      return (
+        <div>
         <ul className="ui-row ui-whitespace">
           <li className="ui-col ui-col-25 modules center-align">
             <a href="#">
@@ -150,12 +150,12 @@ var ModulesComponent = React.createClass({
         </div>
         <div className="line"></div>
       </div>
-    )
-  }
+      )
+    }
 });
 
 var DefaultComponent = React.createClass({
-  render(){
+  render() {
     return (
       <div className="default">
         <ul className="ui-row-flex">
@@ -197,20 +197,20 @@ var DefaultComponent = React.createClass({
 });
 
 var RecommendComponent = React.createClass({
-  getInitialState(){
-    return {
-      DomArray: []
-    }
-  },
-  componentDidMount(){
-    $.get('api/data.json', function (res) {
-      var DomArray = [];
-      for (var i = 0; i < res.length; i++) {
-        // 生成块标题DOM
-        if (res[i].title) {
-          var DomTitle = [];
-          DomTitle.push(
-            <div className="Recommend_title">
+  getInitialState() {
+      return {
+        DomArray: []
+      }
+    },
+    componentDidMount() {
+      $.get('api/data.json', function(res) {
+        var DomArray = [];
+        for (var i = 0; i < res.length; i++) {
+          // 生成块标题DOM
+          if (res[i].title) {
+            var DomTitle = [];
+            DomTitle.push(
+              <div className="Recommend_title">
               <ul className="ui-row-flex ui-whitespace ui-flex ui-flex-align-center">
                 <li className="ui-col ui-col-2">
                   <div className="Recommend_line"></div>
@@ -230,31 +230,32 @@ var RecommendComponent = React.createClass({
                 </li>
               </ul>
             </div>
-          );
-        }
-        // 生成块内容DOM
-        if (res[i].content) {
-          var contentData = res[i].content;
-          var row1 = [], row2 = [];
-          // 遍历生成第一行数据
-          for (var k = 0; k < contentData[0]['row'].length; k++) {
-            row1.push(
-              <li className={"ui-col ui-col-" + contentData[0]['row'][k].flex}>
+            );
+          }
+          // 生成块内容DOM
+          if (res[i].content) {
+            var contentData = res[i].content;
+            var row1 = [],
+              row2 = [];
+            // 遍历生成第一行数据
+            for (var k = 0; k < contentData[0]['row'].length; k++) {
+              row1.push(
+                <li className={"ui-col ui-col-" + contentData[0]['row'][k].flex}>
                 <img src={contentData[0]['row'][k].src} alt=""/>
               </li>
-            );
-          }
-          // 遍历生成第二行数据
-          for (var j = 0; j < contentData[1]['row'].length; j++) {
-            row2.push(
-              <li className={"ui-col ui-col-" + contentData[1]['row'][j].flex}>
+              );
+            }
+            // 遍历生成第二行数据
+            for (var j = 0; j < contentData[1]['row'].length; j++) {
+              row2.push(
+                <li className={"ui-col ui-col-" + contentData[1]['row'][j].flex}>
                 <img src={contentData[1]['row'][j].src} alt=""/>
               </li>
-            );
-          }
-          var DomContent = [];
-          DomContent.push(
-            <div className="Recomment_content">
+              );
+            }
+            var DomContent = [];
+            DomContent.push(
+              <div className="Recomment_content">
               <ul className="ui-row-flex">
                 {row1}
               </ul>
@@ -262,27 +263,27 @@ var RecommendComponent = React.createClass({
                 {row2}
               </ul>
             </div>
-          );
+            );
+          }
+          DomArray.push(DomTitle);
+          DomArray.push(DomContent);
         }
-        DomArray.push(DomTitle);
-        DomArray.push(DomContent);
-      }
-      this.setState({
-        DomArray: DomArray
-      });
-    }.bind(this));
-  },
-  render(){
-    return (
-      <div>
+        this.setState({
+          DomArray: DomArray
+        });
+      }.bind(this));
+    },
+    render() {
+      return (
+        <div>
         {this.state.DomArray}
       </div>
-    )
-  }
+      )
+    }
 });
 
 var FooterComponent = React.createClass({
-  render(){
+  render() {
     return (
       <footer className="ui-footer ui-footer-stable ui-border-t">
         <ul className="ui-tiled">
